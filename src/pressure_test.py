161e9514566a247c1c5b390c6e17620fa9d7aaea
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2024-03-07 15:03:36 -0800

    updates

--- src/pressure_test.py
+++ src/pressure_test.py
@@ -3,29 +3,14 @@ import utime
 import struct
 
 def readPressureSensor():  
-    
     # scan I2C bus to make sure 1 device talking
-    # should output 40 or either 
-    scan1 = I2C_obj.scan() # Check for devices on bus, output is I2C Device Address
-    
-    # pull data
-    byte_amnt = 6
-    data = I2C_obj.mem_read(byte_amnt,scan1[0],0x00)
-    
-
-# # scan I2C bus to make sure 1 device talking
-# scan1 = I2C_obj.scan() # Check for devices on bus, output is I2C Device Address
-# 
-# # got rid of quotes b/c that makes string, we want to send just bin number
-# # 1100b (b on end shouldnt be there, data sheet isnt in python, they think b at end is how to say bin number)
-# # if hex put 0x in front of it
-# # if bin put 0b in front of it
-# I2C_obj.mem_write(0b1100,scan1[0],0x3D) # 0x3D is the register, 1100b is NDOF mode
+    sensor_addr = I2C_obj.scan() # Check for devices on bus, output is I2C Device Address
+    return sensor_addr[0] #element num 0 to get out of list
 
 if __name__ == '__main__':
     
     # init
-    Sensor_Addr = 0x28 # I2C Addr From Data Sheet
+    #Sensor_Addr = 0x28 # I2C Addr From Data Sheet
     
     I2C_obj = pyb.I2C(1,pyb.I2C.CONTROLLER,baudrate=100000)
     
@@ -33,23 +18,19 @@ if __name__ == '__main__':
         utime.sleep (1) # sleep 1 second
         
         try:
+            addr = readPressureSensor()
+
+            byte_array = bytearray(7)
+            # pull data
+            data = I2C_obj.recv(byte_array,addr)
+            
+            # old 
+            # pull status
+            #data = I2C_obj.mem_read(7,addr,0x51)
+            
             pass
 
         except KeyboardInterrupt:
             break
 
-# while True:
-#     utime.sleep (1)  # Sleep for 1 sec
-#     try:
-#         # pull all 
-#         Euler = I2C_obj.mem_read(6,scan1[0],0x1A)
-#         
-#         # unpack
-#         X,Y,Z = struct.unpack('<hhh',Euler)
-#         
-#         # print in degrees
-#         print('Euler (X,Y,Z) =',X/16,Y/16,Z/16)
-#     
-#     except KeyboardInterrupt:
-#         break
-        
+
