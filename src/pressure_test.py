b3116a77577fc65f01f02055b9a130ff7c0e91dc
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2024-03-09 11:43:40 -0800

    put sensor collection into a class

--- src/pressure_test.py
+++ src/pressure_test.py
@@ -2,54 +2,50 @@ import pyb
 import utime
 import struct
 
-def readPressureSensor():  
-    # scan I2C bus to make sure 1 device talking
-    sensor_addr = I2C_obj.scan() # Check for devices on bus, output is I2C Device Address
-    return sensor_addr[0] #element num 0 to get out of list
+class Sensor:
+    
+    def __init__(self,sensor_addr):     
+        
+        self.sensor_addr = sensor_addr
+    
+    def readPressureSensor(self):
+        
+        # pull data
+        byte_array = bytearray(7)
+        data = I2C_obj.recv(byte_array,self.sensor_addr)
+        return data
+        
+        #status = '{0:08b}'.format(data[0])         
+        
+        #pressure = '{0:024b}'.format(data[1])
+       
+        #return status, pressure
 
 if __name__ == '__main__':
-    
-    # init
-    #Sensor_Addr = 0x28 # I2C Addr From Data Sheet
-    
+           
     I2C_obj = pyb.I2C(1,pyb.I2C.CONTROLLER,baudrate=100000)
+
+    # init
+    #Sensor_Addr = 0x28 # I2C Addr From Data Sheet  
+    # scan I2C bus to make sure 1 device talking
+    sensor_addr = I2C_obj.scan() # Check for devices on bus, output is I2C Device Address   
     
+    sensor_obj = Sensor(sensor_addr)
+           
     while True:
         utime.sleep (1) # sleep 1 second
-        
-        try:
-            addr = readPressureSensor()
 
-            byte_array = bytearray(7)
-            # pull data
-            data = I2C_obj.recv(byte_array,addr)
+        try:
+            data = sensor_obj.readPressureSensor()
+            #status, pressure = Sensor(sensor_addr)
             
-            # status = bin(data[0]) # truncates leading zeros
-            status = '{0:08b}'.format(data[0])         
-            #.format exists in the (string?) class and is built in
-            
-            pressure = '{0:024b}'.format(data[1])
+            #print(f'{status=},{pressure=}')
             
             # Write the three bytes,
             # wait 5 ms
             # .read
             
-            
-            
-            # charlie info::
-            #010b
-            # # means print prefix
-            # 0 means wit hleading zeros
-            # 10 means 10 total digs worth of chars
-            # b binary
-            
-            # old 
-            # pull status
-            #data = I2C_obj.mem_read(7,addr,0x51)
-            
             pass
 
         except KeyboardInterrupt:
             break
-
-
